4288eeff1261484f2438fec2885091525cde3c12
mrjinw <mrjinw@naver.com>
2026-03-11 15:08:45 +0900

    Add files via upload
    과제2

---
+++ Task2/Task2/Warrior.h
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include "Player.h"
+#pragma once
+
+class Warrior : public Player
+
+{
+public:
+
+	Warrior(std:: string nickname);
+	void attack() override; // 값을 주는 건 cpp에서 
+};
+
